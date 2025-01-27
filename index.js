c79c81404f4962a358ca1a9eff2ddf6a174a5751
Gyuri <marie.gyuri@gmail.com>
2025-01-27 19:33:37 +0100

    update code

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) =>{
+    a.src = 'https://www.stuttgarter-nachrichten.de/media.media.1eed0918-632a-4962-a09f-fa21b96b78c6.16x9_1024.jpg'
+})
+
+}, 500)
